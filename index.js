19587b6433f9653dcee80ee18e909e54dce2765b
Lucas Minamihara <72841629+LucasMinamihara@users.noreply.github.com>
2022-06-12 22:06:44 -0300

    First Calculator 2

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ number.forEach((eachNumber) => {
     if (choiceNumber.length > 0) {
       let choiceNumberRange = choiceNumber.length;
       console.log(choiceNumberRange);
+      // Need to create a new logical here to string each number of ChoiceNumber Array and then tranform it to a number again 
+      // but i will do it only if the user dont click on symbol, for example if click on minus button i will just agroup the array and do the calaculation with another number clicked by user.
       for (let i = 0; i < choiceNumberRange.length; i++) {
         console.log(choiceNumber[i]);
         valueCalc += choiceNumber[i].toString;
